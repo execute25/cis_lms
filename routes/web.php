129d786052d24b33ab4fc97763d901a803ad1b00
Sergey <sb88@inowing.net>
2023-05-09 10:44:07 +0900

    Auth fix

--- routes/web.php
+++ routes/web.php
@@ -13,11 +13,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::group(['middleware' => ['auth']], function () {
+    Route::get('/', function () {
+        return view('welcome');
+    });
+
 });
 
-//Auth::routes(['register' => false]);
+
+Auth::routes(['register' => false]);
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
